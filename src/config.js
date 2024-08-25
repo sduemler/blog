d553905ba2cc9c4f279535ccd0ec182520c8ddf1
sduemler <samduemler@gmail.com>
2024-08-25 19:53:51 -0400

    Change title and description

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const SITE_TITLE = "Cassidy's blog template";
-export const SITE_DESCRIPTION = "A blog template for you!";
+export const SITE_TITLE = "Sam's Blog!";
+export const SITE_DESCRIPTION = "See title";
