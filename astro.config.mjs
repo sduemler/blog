1a8f1bdcd9da94c1a41be14bf878d8c30b0ac340
sduemler <samduemler@gmail.com>
2024-08-25 21:41:02 -0400

    please show profile pic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
-import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://chocobosam.com/",
-	base: "/",
-	integrations: [sitemap()],
-	markdown: {
-		shikiConfig: {
-			theme: "material-theme-darker",
-			langs: [],
-		},
-	},
+  site: 'https://chocobosam.com/',
+  base: '/',
+  integrations: [sitemap()],
+  markdown: {
+    shikiConfig: {
+      theme: 'material-theme-darker',
+      langs: [],
+    },
+  },
 });
