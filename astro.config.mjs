2bfdb720f0999b3268cf28c47579774776f3c23b
Sam Duemler <samduemler@gmail.com>
2024-09-07 19:52:49 -0400

    Fixing Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,8 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  server: {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
-  },
-  adapter: netlify(),
   site: 'https://chocobosam.com/',
   base: '/',
   integrations: [sitemap()],
