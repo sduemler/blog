1797179ff2e3c7dee51db22868bfe7dbff84c585
Sam Duemler <samduemler@gmail.com>
2026-06-13 10:25:55 -0400

    Updated to use icons instead of custo svg icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://chocobosam.com/',
   base: '/',
-  integrations: [sitemap()],
+  integrations: [sitemap(), icon()],
   markdown: {
     shikiConfig: {
       theme: 'material-theme-darker',
